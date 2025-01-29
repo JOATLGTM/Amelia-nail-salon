980bb9d2b4b4c0a500c56e77bd08ccff03711902
Chris Vo <cv@Chriss-MacBook-Pro.local>
2025-01-28 21:51:19 -0600

    add vercel Speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
 				<div className="relative min-h-screen">
 					{children}
 					<Analytics />
+					<SpeedInsights />
 				</div>
 			</body>
 		</html>
