c10d72565a38bfa5e9dc18d44c0063ce5bac675e
Chris Vo <cv@Chriss-MacBook-Pro.local>
2025-04-11 11:09:27 -0500

    enlarge promo banner

--- components/promo-banner.js
+++ components/promo-banner.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const PromoBanner = () => {
 	return (
-		<div className="w-full bg-gray-100 text-gray-800 text-center py-2">
-			<p className="text-sm font-medium">
+		<div className="w-full bg-gray-100 text-gray-800 text-center py-4 p-4">
+			<p className="text-lg font-medium">
 				Enjoy 20% off to first time customers!
 			</p>
 		</div>
